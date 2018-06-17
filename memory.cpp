4783082ca0220245efbb2b2fee1fb8d2e7c84f38
Madhav Kanbur <abcdjdj@gmail.com>
2018-06-18 02:14:03 +0530

    memory : Add debug info
    Signed-off-by: Madhav Kanbur <abcdjdj@gmail.com>

--- memory.cpp
+++ memory.cpp
@@ -1,4 +1,5 @@
 #include "cpu.h"
+#include <cstdio>
 
 Memory::Memory(CPU *cpu)
 {
@@ -7,6 +8,7 @@ Memory::Memory(CPU *cpu)
 
 uint8_t Memory::cpu_mmap_read(uint16_t addr)
 {
+	printf("Reading addr 0x%x\n", addr);
 	if (addr <= 0x1FFF)
 		return cpu->ram[addr % 0x800];
 	else
@@ -16,6 +18,7 @@ uint8_t Memory::cpu_mmap_read(uint16_t addr)
 void Memory::cpu_mmap_write(uint16_t addr, uint8_t data)
 {
 	/* TODO : Implement memory access to all 64 KB */
+	printf("Writing data=%x to addr 0x%x\n", data, addr);
 	if (addr <= 0x1FFF)
 		cpu->ram[addr & 0x800] = data;
 	else
